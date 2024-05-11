0cefd1b37087f31e77d2cbdaa1ca49d8fab58889
kristina-prog <kristina.birkeli@gmail.com>
2024-05-11 18:08:04 +0200

    Login with crudapi setup

--- js/login.js
+++ js/login.js
@@ -1,15 +1,49 @@
-// crudapi_key: d5Nb8qtaJKK1JpdgwpVbyYPixSqITasMLWj49DqZ89qNYH0tmg
-const apiUrl = "https://crudapi.co.uk/api/v1/{data_type}";
+document.addEventListener("DOMContentLoaded", function () {
+  const crudapiKey = "d5Nb8qtaJKK1JpdgwpVbyYPixSqITasMLWj49DqZ89qNYH0tmg";
+  const apiUrl = "https://crudapi.co.uk/api/v1/";
+  const dataType = "users";
 
-// Login form and button
+  const loginForm = document.getElementById("loginForm");
+  const errorMsg = document.getElementById("errorMsg");
 
-const loginForm = document.getElementById("loginForm");
-const username = document.getElementById("username").value;
-const password = document.getElementById("password").value;
-const errorMsg = document.getElementById("message");
+  // const loginBtn = document.getElementById("loginBtn");
+  // const registerBtn = document.getElementById("registerBtn");
 
-const loginBtn = document.getElementById("loginBtn");
-const registerBtn = document.getElementById("registerBtn");
+  loginForm.addEventListener("submit", async function (event) {
+    event.preventDefault();
+
+    const username = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+
+    try {
+      const response = await fetch(apiUrl + dataType, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: "Bearer" + crudapiKey,
+        },
+        body: JSON.stringify({ username, password }),
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        localStorage.setItem("token", data.token);
+        localStorage.setItem("userId", data.userId);
+      } else {
+        const contentType = response.headers.get("content-type");
+        if (contentType && contentType.includes("application/json")) {
+          const errorData = await response.json();
+          errorMsg.innerText = errorData.message;
+        } else {
+        }
+        errorMsg.innerText = "An unexpected error occurred.";
+      }
+    } catch (error) {
+      console.error("Login failed:", error);
+      errorMsg.innerText = "An error occured during login.";
+    }
+  });
+});
 
 // loginForm.addEventListener("submit");
 
